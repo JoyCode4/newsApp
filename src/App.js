cd6ff33dd1a41dc895cb265f3c76a3dd330ac4e7
JoyCode4 <joywadhonkar4@gmail.com>
2023-05-29 13:46:33 +0530

    created the navbar component and render using class based component of the react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 import React, { Component } from 'react'
 
@@ -6,7 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Hello, Class Based component
+        <Navbar/>
       </div>
     )
   }
